e43294992db8eb6c44a280fd9b20383c51724f94
David Skinner <skinner.david@gmail.com>
2018-05-17 07:00:24 -0500

    Update teapot.go

--- teapot.go
+++ teapot.go
@@ -4,7 +4,7 @@
 //
 // by David Skinner.
 //
-// reference:
+// reference
 //  https://www.ietf.org/rfc/rfc2324.txt
 //  http://www.rfc-editor.org/rfc/rfc7168.txt
 //  http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html
