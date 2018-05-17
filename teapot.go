7a0f318106d88698a90a5908aa1867a4b4d14370
David Skinner <skinner.david@gmail.com>
2018-05-17 06:51:20 -0500

    Added link to wikipedia

--- teapot.go
+++ teapot.go
@@ -8,6 +8,7 @@
 //  https://www.ietf.org/rfc/rfc2324.txt
 //  http://www.rfc-editor.org/rfc/rfc7168.txt
 //  http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html
+//  https://en.wikipedia.org/wiki/Hyper_Text_Coffee_Pot_Control_Protocol#Protocol
 package teapot
 
 import (
